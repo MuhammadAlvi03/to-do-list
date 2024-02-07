96e974c9d5fe36e1e820394f000c2fa2e85085a9
Muhammad Alvi <149120789+MuhammadAlvi03@users.noreply.github.com>
2024-02-06 18:54:40 -0600

    refactor: now returns method for task creation rather than factory function

--- src/modules/taskManager.js
+++ src/modules/taskManager.js
@@ -1,13 +1,27 @@
-/**
- * Creates Task object
- * @param {string} title Title of task
- * @param {string} storage Name of array within storages object that task should be pushed into
- * @param {string} deadline Deadline of task
- */
-export function Task(title, storage = null, deadline = null) {
-    this.title = title;
-    this.status = "To Do";
-    this.deadline =  deadline ? deadline.toLocaleDateString() : null;
-    this.storage = storage ? storage : 'allTasks';
-    this.id = Math.random().toString(16).slice(2);
-}
+const createTaskManager = () => {
+    /**
+     * Creates Task object
+     * @param {string} title Title of task
+     * @param {string} storage Array within storages object that task should be pushed into
+     * @param {string} deadline Deadline of task
+     */
+    function Task(title, storage, deadline) {
+        this.title = title;
+        this.status = "To Do";
+        this.deadline = deadline ? deadline.toLocaleDateString() : null;
+        this.storage = storage ? storage : "All Tasks";
+        this.id = Math.random().toString(16).slice(2);
+    }
+
+    
+    const updateTaskStatus = (task, taskStatus) => {
+        task.status = taskStatus;
+    };
+
+    return {
+        createTask: (title = 'New Task', storage = 'All Tasks', deadline = null) => new Task(title, storage, deadline),
+        updateTaskStatus,
+    };
+};
+
+export default createTaskManager;
