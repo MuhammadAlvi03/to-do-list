0b95293784d759a0a2e1531e6cc4b73325d790c8
Muhammad Alvi <149120789+MuhammadAlvi03@users.noreply.github.com>
2024-02-15 19:19:13 -0600

    feat: add methods to attach event listeners to display elements
    refactor: clean up displayTasks function

--- src/modules/displayController.js
+++ src/modules/displayController.js
@@ -9,6 +9,7 @@ const createDisplayController = (domElements, storageManager) => {
         createProjectSubmitButton,
         
         createTaskButton,
+        createTaskListItem,
         createTaskInputContainer,
         createTaskSubmitButton,
 
@@ -21,6 +22,8 @@ const createDisplayController = (domElements, storageManager) => {
     } = domElements;
     
     const directoryButtonsArr = Array.from(directoryButtons);
+    const mainStorageButtonsArr = [];
+    const projectStorageButtonsArr = [];
     const statusButtonsArr = Array.from(statusButtons);
 
     function changeCurrentDirectory(event) {
@@ -48,9 +51,8 @@ const createDisplayController = (domElements, storageManager) => {
             storageItem.className = 'directory-btn';
             storageItem.textContent = storageName;
             storageItem.id = storageName.replace(' ', '-');
-
-            storageItem.addEventListener('click', directoryButtonClickHandler)
             directoryButtonsArr.push(storageItem);
+            mainStorageButtonsArr.push(storageItem);
 
             storageItemContainer.appendChild(storageItem);
             mainStoragesList.appendChild(storageItemContainer);
@@ -68,9 +70,9 @@ const createDisplayController = (domElements, storageManager) => {
             projectItem.className = 'directory-btn';
             projectItem.textContent = projectName;
             projectItem.id = projectName.replace(' ', '-');
-
-            projectItem.addEventListener('click', directoryButtonClickHandler);
+            attachProjectButtonEventListeners(projectItem);
             directoryButtonsArr.push(projectItem);
+            projectStorageButtonsArr.push(projectItem);
 
             projectItemContainer.appendChild(projectItem);
             projectList.appendChild(projectItemContainer);
@@ -82,32 +84,49 @@ const createDisplayController = (domElements, storageManager) => {
         while (taskList.firstChild !== createTaskInputContainer.parentNode) {
             taskList.removeChild(taskList.firstChild);
         }
-        if (storage.length) {
+        if (storage && storage.length) { // added AND operator
             for (let task of storage) {
                 let taskItem = document.createElement('li');
                 taskItem.className = 'task-item';
     
                 let taskTitleContainer = document.createElement('div');
                 taskTitleContainer.className = 'task-title';
-                taskTitleContainer.textContent = task.title;
+                taskTitleContainer.textContent = task.title || 'Untitled Task';
     
                 let taskStatusContainer = document.createElement('div');
+                taskStatusContainer.className = 'task-status-container';
                 
                 let statusButtonToDo = document.createElement('button'); statusButtonToDo.className = 'status-button to-do-button'; statusButtonToDo.dataset.statusButtonType = 'To Do'; statusButtonToDo.textContent = 'To Do';
                 let statusButtonDoing = document.createElement('button'); statusButtonDoing.className = 'status-button doing-button'; statusButtonDoing.dataset.statusButtonType = 'Doing'; statusButtonDoing.textContent = 'Doing';
                 let statusButtonDone = document.createElement('button'); statusButtonDone.className = 'status-button done-button'; statusButtonDone.dataset.statusButtonType = 'Done'; statusButtonDone.textContent = 'Done';
                 let statusButtonsArr = [];
                 statusButtonsArr.push(statusButtonToDo, statusButtonDoing, statusButtonDone);
+                statusButtonsArr.forEach(button => {
+                    button.addEventListener('click', statusButtonClickHandler);
+                })
     
     
                 let taskDeadlineContainer = document.createElement('div'); taskDeadlineContainer.className = 'task-deadline';
-                taskDeadlineContainer.textContent = task.deadline;
+                taskDeadlineContainer.textContent = task.deadline || 'No Deadline';
+
+                let deleteTaskButton = document.createElement('button'); deleteTaskButton.className = 'delete-task-button';
+                deleteTaskButton.textContent = 'X';
+
+                taskItem.dataset.taskid = task.id;
     
                 taskStatusContainer.append(...statusButtonsArr);
-                taskItem.append(taskTitleContainer, taskStatusContainer, taskDeadlineContainer);
+                taskItem.append(taskTitleContainer, taskStatusContainer, taskDeadlineContainer, deleteTaskButton);
                 taskList.insertBefore(taskItem, createTaskInputContainer.parentNode);
             }
-        } else return;
+        } else {
+            createPlaceholderForEmptyStorage();
+        }
+    }
+
+    function createPlaceholderForEmptyStorage() {
+        let noTasksMessage = document.createElement('span');
+        noTasksMessage.textContent = 'No Tasks';
+        taskList.insertBefore(noTasksMessage, createTaskListItem);
     }
 
     function updateTaskStatus(event) {
@@ -117,6 +136,10 @@ const createDisplayController = (domElements, storageManager) => {
             button.classList.remove('active');
         });
         event.target.classList.add('active');
+    }
+
+    const attachProjectButtonEventListeners = (button) => {
+        button.addEventListener('click', directoryButtonClickHandler);
     }
     
     // click handlers
@@ -162,26 +185,46 @@ const createDisplayController = (domElements, storageManager) => {
         createTaskButton.classList.remove('hidden');
     }
 
+    // wip
     const statusButtonClickHandler = (event) => {
-        updateTaskStatus(event);
+        const taskID = event.target.parentNode.parentNode.dataset.taskid;
+        const storageName = directoryButtonsArr.find(button => {
+            button.classList.contains('active');
+        }).id.replace('-', ' ');
+
+        console.log(taskID);
+        updateTaskStatus(task, taskStatus);
     }
 
 
     // event listeners
 
+    const attachEventListeners = () => {
 
-    directoryButtonsArr.forEach(button => {
-        button.addEventListener('click', directoryButtonClickHandler);
-    })
+        mainStorageButtonsArr.forEach(button => {
+            button.addEventListener('click', directoryButtonClickHandler);
+        })
+    
+        createProjectButton.addEventListener('click', createProjectButtonClickHandler);
+        createProjectSubmitButton.addEventListener('click', createProjectSubmitButtonClickHandler);
+        createTaskButton.addEventListener('click', createTaskButtonClickHandler);
+        createTaskSubmitButton.addEventListener('click', createTaskSubmitButtonClickHandler);
+    
+        statusButtonsArr.forEach(button => {
+            button.addEventListener('click', statusButtonClickHandler);
+        })
 
-    createProjectButton.addEventListener('click', createProjectButtonClickHandler);
-    createProjectSubmitButton.addEventListener('click', createProjectSubmitButtonClickHandler);
-    createTaskButton.addEventListener('click', createTaskButtonClickHandler);
-    createTaskSubmitButton.addEventListener('click', createTaskSubmitButtonClickHandler);
+    }
 
-    statusButtonsArr.forEach(button => {
-        button.addEventListener('click', statusButtonClickHandler);
-    })
+    const initDisplay = () => {
+        displayMainStorages(storages["Main Storages"]);
+        displayProjects(storages["Projects"]);
+        setTimeout(() => {
+            const allTasksButton = document.querySelector("#All-Tasks");
+            
+            allTasksButton.click();
+        }, 0);
+    }
 
 
     return {
@@ -189,6 +232,9 @@ const createDisplayController = (domElements, storageManager) => {
         displayProjects,
         displayTasks,
         getCurrentDirectoryName,
+        attachProjectButtonEventListeners,
+        attachEventListeners,
+        initDisplay,
     }
 }
 
